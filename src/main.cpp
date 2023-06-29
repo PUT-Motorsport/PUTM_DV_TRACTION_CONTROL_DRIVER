19677e2d088d0bde0996c5beda9b5a7aa849b5bf
KozAAAAA <mateusz-koza@outlook.com>
2023-06-29 21:47:29 +0200

    Replace old can library with the new one

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,33 @@
 #include "sensor_msgs/Joy.h"
 #include <iostream>
 
-#include "PUTM_DV_CAN_LIBRARY/Inc/putm_can_interface.hpp"
+#include "PUTM_DV_CAN_LIBRARY_MULTIPLE_SOCKETS/include/can_tx.hpp"
 
 using namespace PUTM_CAN;
 
-CAN can;
+static Apps_main apps_main_frame = {0};
 
-Apps_main frame = {
-  .pedal_position=0,
-  .counter=0,
-  .position_diff=0,
-  .device_state = Apps_states::Normal_operation
-};
-
-void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+void joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
 {
   uint16_t max_val = 100;
-  frame.pedal_position = -(max_val/2)*(joy->axes[4]-1);
-  std::cout<<frame.pedal_position<<"\n";
+  frame.pedal_position = -(max_val / 2) * (joy->axes[4] - 1);
+  std::cout << frame.pedal_position << "\n";
 }
 
 int main(int argc, char **argv)
 {
+  CanTx can("slcan0");
   ros::init(argc, argv, "putm_dv_traction_control_driver");
-
   ros::NodeHandle nh;
   ros::Subscriber joy_sub;
   joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, joyCallback);
-
-  can.connect();
+  
   ros::Rate r(100);
   while (ros::ok)
   {
-    can.transmit(frame);
+    can.transmit(apps_main_frame);
     frame.counter++;
     ros::spinOnce();
     r.sleep();
   }
-
 }
-
